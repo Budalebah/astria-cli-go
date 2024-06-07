60b873f00d9f0a012ca684a93abf3bb880fc1819
Jesse Snyder <jessetsnyder@gmail.com>
2024-06-06 22:46:30 -0600

    more sensible default interacting with bridge

--- cmd/sequencer/constants.go
+++ cmd/sequencer/constants.go
@@ -5,6 +5,6 @@ import "github.com/astria/astria-cli-go/cmd"
 const (
 	DefaultSequencerURL     = "http://127.0.0.1:26657"
 	DefaultSequencerChainID = cmd.DefaultLocalSequencerChainID
-	DefaultBridgeAssetID    = "transfer/channel-0/utia"
+	DefaultBridgeAssetID    = "nria"
 	DefaultBridgeFeeAssetID = "nria"
 )
